df7851abe6abe0126def34a895d41dc5b5e10328
vianismee <104370516+vianismee@users.noreply.github.com>
2025-05-14 20:41:46 +0700

    Feat add Feat Path Random Code (#5)
    * add feat Verify Page
    
    * Product Verify Page
    
    * Add feature random path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,30 @@
 import { NextResponse, type NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-    return NextResponse.redirect(new URL("/link/verify", request.url))
-}
+function generateRandomCode(length: number) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; // Karakter yang digunakan
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      result += chars[randomIndex];
+    }
+    return result;
+  }
+
+  export function middleware(request: NextRequest) {
+    // Ambil URL asal
+    const url = new URL(request.url);
+  
+    // Buat angka dan huruf acak untuk parameter 'code'
+    const randomCode = generateRandomCode(10); // Bisa disesuaikan panjangnya
+  
+    // Tentukan path baru dengan menambahkan query parameter 'code'
+    url.pathname = '/link/verify';
+    url.searchParams.set('certificate-item', randomCode); // Tambahkan parameter 'code'
+  
+    // Redirect ke URL baru dengan parameter acak
+    return NextResponse.redirect(url);
+  }
 
 export const config = {
     matcher: "/link",
-};
+  };
