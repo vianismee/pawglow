0f1fb201508d6e8e467e9ccc661e0b1ffe12b722
chevian <chevianbs@gmail.com>
2025-05-14 16:56:57 +0700

    add feat Verify Page

---
+++ src/app/(link)/link/verify/components/MobilePage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MobilePage = () => {
+  return (
+    <main className="flex justify-center">Your Product Authentivication</main>
+  );
+};
+
+export default MobilePage;
